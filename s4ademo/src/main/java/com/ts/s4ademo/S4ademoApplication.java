c42b79b39d8f6d6a5a40aa0f84234a13f42a35f8
tomasz.sobiecki <tomasz.sobiecki@dsv.com>
2021-02-13 09:12:14 +0100

    Initial commit

---
+++ s4ademo/src/main/java/com/ts/s4ademo/S4ademoApplication.java
@@ -0,0 +1,13 @@
+package com.ts.s4ademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4ademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4ademoApplication.class, args);
+	}
+
+}
---
+++ s4ademo/src/test/java/com/ts/s4ademo/S4ademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.s4ademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4ademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
